bddd402f5cb5caf62a04b95915dae682cd79f98d
Williams, Richard <rowwor@bellsouth.net>
2019-08-04 13:12:05 -0500

    Revised localhost info.

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host:'localhost',
+    host:'127.0.0.1',
     user:'root',
     password:'rowwebdevelop2019$',
     database:'burgers_db'
