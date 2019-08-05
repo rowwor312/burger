33e4c93698a95f74f46aee02f5a404f3eb261170
Williams, Richard <rowwor@bellsouth.net>
2019-08-05 10:41:03 -0500

    Modified Connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,17 @@
 var mysql = require('mysql');
 // var connection;
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+// if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+  var connection = mysql.createConnection({
+    port: 3306,
     host:'localhost',
     user:'root',
     password:'rowwebdevelop2019$',
     database:'burgers_db'
   });
-}
+// }
 
 // Establish connection
 connection.connect(function(err) {
