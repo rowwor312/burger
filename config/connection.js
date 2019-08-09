62c30216885d908b5f63e6983f7a346c6c711171
Williams, Richard <rowwor@bellsouth.net>
2019-08-09 10:55:05 -0500

    Revised connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,26 @@
 // Creating connection to mySQL database
 var mysql = require('mysql');
-// var connection;
+var connection;
 
-// if (process.env.JAWSDB_URL){
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-  var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'rowwebdevelop2019$',
+    password:'password',
     database:'burgers_db'
   });
-// }
+}
 
 // Establish connection
 connection.connect(function(err) {
   if (err) {
-    console.error("Error Connecting: " + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("Connected as Id: " + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
-// Export
+// Export.
 module.exports = connection;
