2272d87e102058f8f5bdb690a0e583105605253a
Williams, Richard <rowwor@bellsouth.net>
2019-08-09 11:05:29 -0500

    Updated Password.

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL){
   connection = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'password',
+    password:'rowwebdevelop2019$',
     database:'burgers_db'
   });
 }
